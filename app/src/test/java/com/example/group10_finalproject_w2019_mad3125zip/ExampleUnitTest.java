3587f56c0f4b2c2ca73980579d011f7c0f3e2c34
sooraj <soorajsuneesh@gmail.com>
2019-04-02 20:38:58 -0400

    Initial commit

---
+++ app/src/test/java/com/example/group10_finalproject_w2019_mad3125zip/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.group10_finalproject_w2019_mad3125zip;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
